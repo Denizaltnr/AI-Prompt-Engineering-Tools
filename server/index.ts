e3d9e0e88c4d8e2eaf1b568d11c8bc61f6ef8486
Deniz Altuner <skinsbetscom27@gmail.com>
2026-03-24 23:22:00 +0300

    Update server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
+import { setupAuth } from "./replit_integrations/auth";
 
 const app = express();
 const httpServer = createServer(app);
@@ -63,6 +64,7 @@ app.use((req, res, next) => {
   const { seedDatabase } = await import("./seed");
   await seedDatabase();
 
+  await setupAuth(app);
   await registerRoutes(httpServer, app);
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
